2c3ee6e267868a4a13de2e3cffeb69a665aa6715
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2026-06-26 10:59:21 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # print (nums)
+        for fe in nums:
+            for se in nums:
+                if fe+se == target and nums.index(fe)!=nums.index(se):
+                    return [nums.index(fe),nums.index(se)]
